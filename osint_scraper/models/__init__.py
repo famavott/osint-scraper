455dbd8bef026c2ab8c9a2bfc9c2b386644f47bd
Chris Closser <chrisclosser1@gmail.com>
2017-11-13 11:34:17 -0800

    Git Hub Model

--- osint_scraper/models/__init__.py
+++ osint_scraper/models/__init__.py
@@ -1,3 +1,6 @@
+"""Init models."""
+
+
 from sqlalchemy import engine_from_config
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import configure_mappers
@@ -5,7 +8,7 @@ import zope.sqlalchemy
 
 # import or define all models here to ensure they are attached to the
 # Base.metadata prior to any initialization routines
-from .mymodel import MyModel  # flake8: noqa
+from .gitmodel import GHModel  # flake8: noqa
 
 # run configure_mappers after defining all of the models to ensure
 # all relationships can be setup
