463723de213cda0a708ac771b58c2ecf9f661437
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-14 11:05:41 -0800

    Moved recon functions to single script. Moved helper scrape functions to same script, added some error handling.

--- osint_scraper/scripts/pwned.py
+++
@@ -1,8 +0,0 @@
-"""Check HIBP."""
-import pypwned
-
-
-def pwned_recon(email):
-    """Check HIBP if email has been compromised."""
-    results = pypwned.getAllBreachesForAccount(email=email)
-    return results
