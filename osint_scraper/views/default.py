a5159d5b1aea3f27b5b658ea9a0930aca9f235c5
Matt Favoino <mattfavoino@gmail.com>
2017-11-16 14:39:44 -0800

    Twitter data now working after edits to jinja and recon.

--- osint_scraper/views/default.py
+++ osint_scraper/views/default.py
@@ -4,7 +4,7 @@ from pyramid.view import view_config
 
 from ..scripts.recon_handler import recon_handler
 
-from ..scripts.dummy_recon import dummy_recon
+# from ..scripts.dummy_recon import dummy_recon
 
 
 @view_config(route_name='home', renderer='../templates/home.jinja2')
@@ -20,5 +20,5 @@ def results_view(request):
         return {}
     user_name = request.params['handle']  # pragma: no cover
     email = request.params['email']  # pragma: no cover
-    # return recon_handler(user_name, email)  # pragma: no cover
-    return dummy_recon(user_name, email)  # pragma: no cover
+    return recon_handler(user_name, email)  # pragma: no cover
+    # return dummy_recon(user_name, email)  # pragma: no cover
