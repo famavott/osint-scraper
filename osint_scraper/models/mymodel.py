53269e284876415228d51b7be81620c305a2f7ad
Matt Favoino <mattfavoino@gmail.com>
2017-11-16 18:43:13 -0800

    Commented out imports

--- osint_scraper/models/mymodel.py
+++ osint_scraper/models/mymodel.py
@@ -1,18 +1,18 @@
-from sqlalchemy import (
-    Column,
-    Index,
-    Integer,
-    Text,
-)
+# from sqlalchemy import (
+#     Column,
+#     Index,
+#     Integer,
+#     Text,
+# )
 
-from .meta import Base
-
-
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+# from .meta import Base
 
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+# class MyModel(Base):
+#     __tablename__ = 'models'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(Text)
+#     value = Column(Integer)
+
+
+# Index('my_index', MyModel.name, unique=True, mysql_length=255)
