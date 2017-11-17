f20df7e8000e18cc32b6361eab25ef05598ea183
Chris Closser <chrisclosser1@gmail.com>
2017-11-17 11:41:11 -0800

    test with tom for hacked emails

--- osint_scraper/conftest.py
+++ osint_scraper/conftest.py
@@ -1,38 +1,17 @@
 """Fixture and request setup for test."""
 
-import os
 
 from pyramid.config import Configurator
-
-from pyramid.testing import DummyRequest
-
-from pyramid import testing
 
 import pytest
 
 from osint_scraper.scripts.recon_handler import recon_handler
-
-from osint_scraper.scripts.recon import (facebook_recon,
-                                         flickr_recon,
-                                         github_recon,
-                                         hacked_email_recon,
-                                         hacker_recon,
-                                         imgur_recon,
-                                         liveleak_recon,
-                                         photobucket_recon,
-                                         pwned_recon,
-                                         steam_recon,
-                                         twitter_recon,
-                                         wikipedia_recon,
-                                         youtube_recon)
 
 
 def main(global_config, **settings):
     """Function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    # config.include('.vendor')
-    # config.include('.models')
     config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
@@ -78,3 +57,9 @@ def jf_name_example(scope="session"):
 def matt_name_example(scope="session"):
     """."""
     return recon_handler(user_name='matt')
+
+
+@pytest.fixture
+def tom_email_example(scope="session"):
+    """."""
+    return recon_handler(email='tom@gmail.com')
