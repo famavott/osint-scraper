4949bf99970cee78b0e74c9adc9725fc3a328083
Matt Favoino <mattfavoino@gmail.com>
2018-01-09 14:21:47 -0800

    Removed corpse code in app.js

--- osint_scraper/static/app.js
+++ osint_scraper/static/app.js
@@ -10,40 +10,6 @@ function loading_page(){
     $('.progress').fadeIn('slow');
 }
 
-// $('input[name=email]').keyup(function(){
-//     if (this.value.length > 0) {
-//         $('input[class="email-box"]').prop('disabled', false)
-//         $('input[class="all-box"]').prop('disabled', false)
-//     } else {
-//         $('input[class="email-box"]').prop('disabled', true)
-//         $('input[class="all-box"]').prop('disabled', true)
-//     }
-// })
-
-// $('input[name=handle]').keyup(function(){
-//     if (this.value.length > 0) {
-//         $('input[class="handle-box"]').prop('disabled', false)
-//         $('input[class="all-box"]').prop('disabled', false)
-//     } else {
-//         $('input[class="handle-box"]').prop('disabled', true)
-//         $('input[class="all-box"]').prop('disabled', true)
-//     }
-// })
-
-// $('input[name="select_all"]').change(function(){
-//     if(this.checked){
-//         if($('input[id="inputHandle"]').val() !='') {
-//             $('input[class="handle-box"]').prop('checked', true)
-//         }
-//         if($('input[id="inputEmail"]').val() !='') {
-//             $('input[class="email-box"]').prop('checked', true)
-//         }
-//         else {
-//             $('input[name="selected_sites"]').removeattr('checked')
-//         }
-//     }
-// })
-
 $('input[name="select_all"]').change(function(){
     if(this.checked){
         $('input[name="selected_sites"]').prop('checked', true)
